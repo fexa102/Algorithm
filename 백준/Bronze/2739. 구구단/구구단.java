9740d741b72dee0fe765c7a2b905eea3630d75d2
fexa102 <yihjh726@icloud.com>
2024-09-26 08:48:39 +0900

    [Bronze V] Title: 구구단, Time: 196 ms, Memory: 18644 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,21 @@
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int i = 1;
+		
+		for (i = 1; i < 10; i++) {
+			System.out.println(N + " * " + i + " = " + N*i);
+		}
+		
+		
+
+	}
+
+}
