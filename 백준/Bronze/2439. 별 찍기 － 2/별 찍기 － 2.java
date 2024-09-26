1b34d0814cee976e0b8a5682482cc6bb71b5fe32
fexa102 <yihjh726@icloud.com>
2024-09-27 08:49:13 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 272 ms, Memory: 19052 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		
+		for ( int i = 1; i <= a; i++) {
+			for ( int k = i; k <= a - 1; k++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+		}
+		System.out.println();
+
+	}
+
+}
+}
+
