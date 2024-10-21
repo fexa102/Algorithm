d34498ba5a1906b0f88145ea5d588f0670c5f560
fexa102 <yihjh726@icloud.com>
2024-10-22 08:03:03 +0900

    [Bronze V] Title: 초코바, Time: 180 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		if (100*N >= M) {
+			System.out.println("Yes");
+		}
+		
+		else {
+			System.out.println("No");
+		}
+
+	}
+
+}
